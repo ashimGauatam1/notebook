4d1838505b1b6ff429ecea65ed0ab2769f1b185f
ramchandraGautam1 <ramchandra.nesdo@gmail.com>
2024-05-16 21:57:41 +0545

    thursday

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Components/About';
 import Login from './Components/Routes/Login/Login';
 import Signup from './Components/Routes/Signup/Signup';
 import NoteState from './Context/Notestate';
+import Alert from './Components/alert/Alert';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
     <NoteState>
         <BrowserRouter>
+        <Alert message="hello"/>
           <Routes>
           <Route exact path="/" element={<Home />} />
           <Route  path="/about" element={<About />} />
