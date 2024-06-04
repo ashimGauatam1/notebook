1a40a642eb04f54a2b7c3107aba8a7d52122ec7d
ramchandraGautam1 <ramchandra.nesdo@gmail.com>
2024-06-04 20:32:18 +0545

    done

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <Routes>
           <Route exact path="/" element={<Home />} />
           <Route  path="/about" element={<About />} />
-          <Route exact path="/signup" element={<Signup/>} />
+          {/* <Route exact path="/signup" element={<Signup/>} /> */}
           <Route  path="/login" element={<Login onLogin={handlelogin}/>} />
           <Route path='/add_note' element={<Add authToken={authToken} isAuthenticated={isAuthenticated}/>}/>
           <Route path='/notelist' element={<NotesList authToken={authToken} isAuthenticated={isAuthenticated}/>} />
