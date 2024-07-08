edbc90238679b85ae69dcd1cddbcaf9141833cdd
ramchandraGautam1 <ramchandra.nesdo@gmail.com>
2024-07-08 19:01:53 +0545

    added

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -4,9 +4,6 @@ import Notes from '../Card/Card';
 import Alert from '../alert/Alert';
 
 const NotesList = ({ authToken,isAuthenticated }) => {
-  const [notes, setNotes] = useState([]);
-  const [displayAlert, setDisplayAlert] = useState(false);
-  const [alertType,setAlertType]=useState('danger');
 
   useEffect(() => {
     const fetchNotes = async () => {
